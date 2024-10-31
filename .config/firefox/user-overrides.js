f0b5e1cc042aaff79480c325343261294eada3df
James Monk <90908782+jmsedwrdmnkme@users.noreply.github.com>
2024-10-31 11:10:09 +0000

    Removal of dmenu websearch graphical glitching browser window overlay interface.
    Wallpaper script update for higher res wallpapers, and associated new
    xresources with new wallpaper fetched.
    
    Firefox removal of Arkenfox overrides for more vanilla experience.

--- .config/firefox/user-overrides.js
+++ .config/firefox/user-overrides.js
@@ -1,4 +1,4 @@
-user_pref("privacy.resistFingerprinting", true);
-user_pref("privacy.resistFingerprinting.letterboxing", true);
-user_pref("webgl.disabled", true);
-user_pref("privacy.spoof_english", 2);
+// user_pref("privacy.resistFingerprinting", true);
+// user_pref("privacy.resistFingerprinting.letterboxing", true);
+// user_pref("webgl.disabled", true);
+// user_pref("privacy.spoof_english", 2);
