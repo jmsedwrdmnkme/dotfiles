3c4a9139c261ce2035e9b0130d3c814b1c514b6c
James Monk <90908782+jmsedwrdmnkme@users.noreply.github.com>
2025-02-06 17:28:16 +0000

    Clean up unused overrides for User.js in Firefox

--- .config/firefox/user-overrides.js
+++
@@ -1,4 +0,0 @@
-// user_pref("privacy.resistFingerprinting", true);
-// user_pref("privacy.resistFingerprinting.letterboxing", true);
-// user_pref("webgl.disabled", true);
-// user_pref("privacy.spoof_english", 2);
