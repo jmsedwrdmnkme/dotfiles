1376425591fad2953926f7c9e4ae79e8a3f40cfe
James Monk <90908782+jmsedwrdmnkme@users.noreply.github.com>
2026-08-01 12:06:39 +0100

    Ensuring Firefox clears all history on close/reopen

---
+++ .config/mozilla/firefox/Default User/user-overrides.js
@@ -0,0 +1,11 @@
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", true);
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", true);
+user_pref("privacy.clearOnShutdown_v2.downloads", true);
+
+user_pref("privacy.clearSiteData.cookiesAndStorage", true);
+user_pref("privacy.clearSiteData.historyFormDataAndDownloads", true);
+user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", true);
+
+user_pref("privacy.clearHistory.cookiesAndStorage", true);
+user_pref("privacy.clearHistory.historyFormDataAndDownloads", true);
+user_pref("privacy.clearHistory.browsingHistoryAndDownloads", true);
